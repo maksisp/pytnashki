a5686a8e378299c834652935d31d4ecdae17efce
trplx666 <russiansoveet@gmail.com>
2024-05-11 15:27:13 +0300

    main menu buttons

--- mainmenu.py
+++ mainmenu.py
@@ -5,15 +5,32 @@ screen = pygame.display.set_mode((400, 500))
 def main_menu():
     screen.fill((255, 255, 255))
     font = pygame.font.Font(None, 50)
-    text = font.render("Главное меню", True, (0,0,0))
+    text = font.render("Main menu", True, (0,0,0))
     text_rect = text.get_rect(center=(200, 200))
     screen.blit(text, text_rect)
 
-    start_button = pygame.Rect(150, 300, 100, 50)
+    start_button = pygame.Rect(150, 250, 100, 50)
     pygame.draw.rect(screen, (255,0,0), start_button)
     font = pygame.font.Font(None, 36)
     text = font.render("Start", True, (0,0,0))
     text_rect = text.get_rect(center=start_button.center)
+    screen.blit(text, text_rect)
+
+
+
+    settings_button = pygame.Rect(150, 320, 100, 50)
+    pygame.draw.rect(screen, (255,0,0), settings_button)
+    font = pygame.font.Font(None, 36)
+    text = font.render("Settings", True, (0,0,0))
+    text_rect = text.get_rect(center=settings_button.center)
+    screen.blit(text, text_rect)
+
+
+    exit_button = pygame.Rect(150, 380, 100, 50)
+    pygame.draw.rect(screen, (255,0,0), exit_button)
+    font = pygame.font.Font(None, 36)
+    text = font.render("Exit", True, (0,0,0))
+    text_rect = text.get_rect(center=exit_button.center)
     screen.blit(text, text_rect)
 
     pygame.display.update()
