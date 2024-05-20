c27b9c90ecfe9ca2cdb5267395dd09d05d48a020
maksisp <kirillovich.maks@mail.ru>
2024-05-21 01:58:17 +0300

    add game time

--- main.py
+++ main.py
@@ -16,15 +16,21 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 
-#game_board =  [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, ' '], [13, 14, 15, 12]] # проверка проверки на победу
-game_board = random_board()
+game_board =  [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, ' '], [13, 14, 15, 12]] # проверка проверки на победу
+#game_board = random_board()
 save_board =  [[]]
 music_filled = False
 sound_filled = False
 win_count = 0
 moves = 0
 saved = 0
-game_active = True
+time = 1
+game_time = 0
+minuti = 0
+time_cikl = 0
+game_active = True  
+
+
 pygame.mixer.music.set_volume(0.2)
 move_sound = pygame.mixer.Sound('./sounds/свайп.mp3')
 reload_sound = pygame.mixer.Sound('./sounds/reload.wav')
@@ -33,7 +39,7 @@ pygame.mixer.music.load('./sounds/фон.mp3')
 pygame.mixer.music.play(-1)
 
 def run_game(game_board):
-    global main_menu_active , moves , win_count , saved , save_board
+    global main_menu_active , moves , win_count , saved , save_board , time , game_time , minuti , time_cikl
     game_active = True
     
     if win_count == 1:
@@ -44,10 +50,11 @@ def run_game(game_board):
     if saved == 1:
         game_board = save_board
         saved = 0
-        
-
-
-        
+    
+         
+    if time >100:
+        game_time = time // 100
+    
 
     running = True
     while running:
@@ -72,6 +79,9 @@ def run_game(game_board):
                     reload_sound.play()
                     moves = 0
                     game_active = True
+                    time = 0
+                    game_time = 0
+                    time_cikl = 0
             if event.type == pygame.KEYDOWN:   
                 if event.key == pygame.K_ESCAPE:
                     save_board = game_board
@@ -86,10 +96,13 @@ def run_game(game_board):
                 running = False
                 victory_sound.set_volume(0.3)
                 victory_sound.play()
-                win_menu_button = win_menu(screen, moves) 
+                win_menu_button = win_menu(screen, moves, minuti, game_time) 
                 win_count =  1
                 win_menu_active = True
                 moves = 0
+                time = 0
+                game_time = 0
+                time_cikl = 0
                 while win_menu_active:
                     for event in pygame.event.get():
                         if event.type == pygame.QUIT:
@@ -113,13 +126,34 @@ def run_game(game_board):
                     text = font.render(str(game_board[i][j]), True, black)
                     text_rect = text.get_rect(center=(j * 100 + 50, i * 100 + 50))
                     screen.blit(text, text_rect)
+                    
+                    
+        time +=1
+        game_time +=1  
+        game_time = time // 60
+        if game_time % 60 == 0 and game_time > 2 and time_cikl == 0 or time_cikl >= 1:
+            timer1 = font.render(f"{minuti}"f":{game_time}", True, black)
+            timer_rect1 = timer1.get_rect(center=(95, 450))
+            screen.blit(timer1, timer_rect1)
+            time_cikl += 1     
+        elif time_cikl == 0:    
+            timer = font.render(f"{game_time}", True, black)
+            timer_rect = timer.get_rect(center=(100, 450))
+            screen.blit(timer, timer_rect)
+        if time >=60 and game_time >= 60:
+            time = 0
+            game_time = 0
+            minuti += 1 
+            
 
+        
+        
         font = pygame.font.Font(None, 36)
         text = font.render(f"Ходы: {moves}", True, black)
-        text_rect = text.get_rect(center=(200, 450))
+        text_rect = text.get_rect(center=(300, 450))
         screen.blit(text, text_rect)
         pygame.display.update()
-
+       
 
 def run_settings():
     global main_menu_active, music_filled, sound_filled
--- winmenu.py
+++ winmenu.py
@@ -1,6 +1,6 @@
 import pygame
 
-def win_menu(screen, moves):
+def win_menu(screen, moves , minuti , game_time):
     screen.fill((255, 255, 255))
     font_title = pygame.font.Font(None, 50)
     font_text = pygame.font.Font(None, 36)
@@ -12,6 +12,10 @@ def win_menu(screen, moves):
     text_moves = font_text.render(f"Количество ходов: {moves}", True, (0, 0, 0))
     text_moves_rect = text_moves.get_rect(center=(200, 200))
     screen.blit(text_moves, text_moves_rect)
+    
+    text_time = font_text.render(f"Ваше время: {minuti}"f":{game_time}", True , (0, 0, 0))
+    text_time_rect = text_time.get_rect(center = (200, 150))
+    screen.blit(text_time, text_time_rect)
 
     return_button = pygame.Rect(100, 300, 200, 50)
     pygame.draw.rect(screen, (0, 0, 0), return_button, 2)
